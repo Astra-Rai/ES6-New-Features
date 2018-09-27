8d1edcc454b650f5d90ad220ec5c7f3bbc7f469c
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-09-26 20:00:50 -0700

    ES6-New-Features-Tutorial-1-Let

---
+++ ES6-New-Features-Tutorial-1-Let.js
@@ -0,0 +1,34 @@
+//Tutorial on let
+
+
+let movie = 'Good Will Hunting';
+
+function theNoteBook(){
+  let movie = 'The Notebook';
+  //note scope
+  return movie;
+  
+}
+
+//console.log(movie);
+//expected output: 'Good Will Hunting'
+//console.log(theNoteBook());
+//above:the accesses of movie variable w/in thes scope of function
+//theNotebook()
+//expected output: "The Notebook"
+
+function buckyFunction(){
+  let isHorse = true;
+  let saying = 'Bacon is good';
+ 
+  console.log('\nBefore if:', saying);
+//buckyFunction();
+//expected output: "Bacon is good"
+  if(isHorse){  
+    let saying = 'I am a horse';
+    console.log('Inside if:,' saying);
+    } 
+      console.log(saying);
+      //no access so saying won't print
+}
+//insure why code isn't working
