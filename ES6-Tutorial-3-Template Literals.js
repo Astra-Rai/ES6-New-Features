912605b0385850e1dc509ac4a56222fcf47bb3cb
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2018-10-06 12:53:19 -0700

    ES6-Template-Literals

---
+++ ES6-Tutorial-3-Template Literals.js
@@ -0,0 +1,20 @@
+//template literals
+
+let name = "Scooby Doo";
+//instead of ' + name + ', use ${name}, 
+//console.log(`My favorite person is ${name}`);
+
+//template literals allows one to include snippes of javascript i
+
+//another examle
+let a = 5;
+let b = 7;
+
+//console.log(`My favorite person is ${a + b} because he smells good`);
+//notice above that you can add the vaules of a and b here, bu t
+//is not recommended. one should always seperate logic for formatting
+//so you'd have:
+let e = 5; 
+let f = 7; 
+let g = e + f; 
+//console.log(`the sum of e and f is ${g}`);
